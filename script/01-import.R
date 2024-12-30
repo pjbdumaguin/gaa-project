2e6d3ba23535c40da51720cd23aee302de9799c5
pjbdumaguin <dumaguinprincejustin@gmail.com>
2024-12-31 02:08:26 +0800

    fetching dl_links now okay

--- script/01-import.R
+++ script/01-import.R
@@ -3,10 +3,10 @@
 library(tidyverse)
 library(rvest)
 
-base_page <- "https://www.dbm.gov.ph/index.php/"
+base_page <- "https://www.dbm.gov.ph"
 
 # Doesn't work. Some fiscal years have a slightly different link.
-gaa_pages <- paste0(base_page, seq(2020, 2024),
+gaa_pages <- paste0(base_page, "/index.php/", seq(2020, 2024),
                    "/general-appropriations-act-gaa-fy-", seq(2020, 2024))
 
 # Function that takes all the links of a page then
@@ -21,9 +21,11 @@ get_links <- function(page, pattern) {
 }
 
 # This works.
-gaa_pages <- get_links(page = paste0(base_page, "budget"),
-                       pattern = "\\/general\\-appropriations")
+gaa_pages <- get_links(page = paste0(base_page, "/index.php/budget"),
+                       pattern = "\\/general\\-appropriations") |> 
+  {\(x) paste0(base_page, x)}()
 
-dl_links <- map_chr(gaa_pages, get_links(.x, "csv|xlsx?") |> first())
+dl_links <- map_chr(gaa_pages,
+                    \(page) get_links(page, "csv|xlsx?") |> first())
 
   
