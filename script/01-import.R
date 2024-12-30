6e707e2317ecd14cb246beb24b3fcc52899e8649
pjbdumaguin <dumaguinprincejustin@gmail.com>
2024-12-31 01:25:19 +0800

    Making progress to reproducible data source

---
+++ script/01-import.R
@@ -0,0 +1,29 @@
+#'https://www.dbm.gov.ph/
+
+library(tidyverse)
+library(rvest)
+
+base_page <- "https://www.dbm.gov.ph/index.php/"
+
+# Doesn't work. Some fiscal years have a slightly different link.
+gaa_pages <- paste0(base_page, seq(2020, 2024),
+                   "/general-appropriations-act-gaa-fy-", seq(2020, 2024))
+
+# Function that takes all the links of a page then
+# filters it with matching patterns.
+get_links <- function(page, pattern) {
+  Sys.sleep(2) # if called multiple times
+  page |>
+    read_html() |> 
+    html_elements("a") |> 
+    html_attr("href") |> 
+    str_subset(pattern)
+}
+
+# This works.
+gaa_pages <- get_links(page = paste0(base_page, "budget"),
+                       pattern = "\\/general\\-appropriations")
+
+dl_links <- map_chr(gaa_pages, get_links(.x, "csv|xlsx?") |> first())
+
+  
