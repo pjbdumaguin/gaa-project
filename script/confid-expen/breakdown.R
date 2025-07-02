507ca024af627265010aaf8af43b356741ffb0ca
pjbdumaguin <dumaguinprincejustin@gmail.com>
2025-07-03 05:00:52 +0800

    Update treemap implementation
    Shift from heavy reliance on treemapify to geom_rect for better control over treemap aesthetics and layout.

--- script/confid-expen/breakdown.R
+++ script/confid-expen/breakdown.R
@@ -7,8 +7,10 @@ library(treemapify)
 library(ggfx)
 library(glue)
 library(scales)
-library(marquee)
 library(systemfonts)
+library(marquee)
+library(ggfittext)
+library(ggrepel)
 
 # Wrangle data ------------------------------------------------------------
 
@@ -19,8 +21,8 @@ confid_expen <- gaa |>
   select(yr = YEAR, ends_with("DSC"), AMT) |>
   filter(UACS_SOBJ_DSC == "Confidential Expenses", AMT > 0) |>
   summarise(
-    amt = sum(AMT, na.rm = TRUE) * 1e3,
     # data is in thousand
+    amt = sum(AMT, na.rm = TRUE) * 1e3,
     .by = c(yr, UACS_DPT_DSC, UACS_AGY_DSC)
   ) |>
   collect()
@@ -36,20 +38,14 @@ confid_expen <- confid_expen |>
   ) |>
   mutate(
     agy = case_when(
-      str_detect(agency, "Secretary") ~ "OSEC",
+      str_detect(agency, "Secretary") ~ "OSec",
       str_detect(agency, "Customs") ~ "BOC",
       str_detect(agency, "Ombudsman") ~ "OMB",
       str_detect(agency, "Senate") ~ agency,
-      str_detect(agency, "President's") ~ "The President's Offices",
       is.na(agy) ~ str_remove_all(agency, "[^A-Z]"),
       .default = agy
     )
-  ) |>
-  mutate(dpt = case_when(
-    str_detect(department, " President") ~ "O. President",
-    str_detect(department, "Executive") ~ "Other EOs",
-    .default = dpt
-  ))
+  )
 
 # Label -------------------------------------------------------------------
 
@@ -80,6 +76,17 @@ speckle <- function(x, colour, proportion) {
   x
 }
 
+# Marquee style set -------------------------------------------------------
+
+cust_style <- modify_style(
+  classic_style(),
+  "a",
+  weight = "bold",
+  color = "white",
+  background = "#fa7405",
+  padding = trbl(em(0.01))
+)
+
 # Plot --------------------------------------------------------------------
 
 gen_plots <- function(fiscal_year) {
@@ -90,50 +97,164 @@ gen_plots <- function(fiscal_year) {
     mutate(amt_dpt = sum(amt)) |> # for labeling purposes
     ungroup()
   
-  p <- ggplot(ce, aes(
-    area = amt,
-    fill = dpt,
-    label = agy,
-    subgroup = paste0(dpt, "\n", to_php(amt_dpt)),
-  )) +
-    with_custom(
-      geom_treemap(
-        color = "white",
-        size = 1,
-        linetype = "dotted",
-        show.legend = FALSE,
-        start = "topleft",
-        layout = "srow"
+  tm_ce <- treemapify(
+    ce,
+    area = "amt",
+    subgroup = "amt_dpt",
+    layout = "srow",
+    start = "topleft"
+  ) |>
+    mutate(agy_n = n(), .by = dpt) |>
+    mutate(width = xmax - xmin, height = ymax - ymin)
+  
+  tm_ce_subgroup <- tm_ce |>
+    summarise(
+      xmin = min(xmin),
+      xmax = max(xmax),
+      ymin = min(ymin),
+      ymax = max(ymax),
+      .by = c(department, dpt, amt_dpt)
+    ) |>
+    mutate(
+      width = xmax - xmin,
+      height = ymax - ymin,
+      x = xmin + ((xmax - xmin) / 2),
+      y = ymin + ((ymax - ymin) / 2)
+    ) |>
+    left_join(enframe(tile_colrs, "dpt", "color"), by = "dpt")
+  
+  p <- tm_ce |>
+    ggplot(aes(
+      xmin = xmin,
+      xmax = xmax,
+      ymin = ymin,
+      ymax = ymax
+    )) +
+    # agency border
+    geom_rect(
+      aes(fill = dpt),
+      color = "gray90",
+      linetype = "dotted",
+      show.legend = FALSE
+    ) +
+    # agency label
+    geom_fit_text(
+      aes(
+        label = case_when(
+          str_detect(agy, "TPO") ~ agency,
+          str_equal(dpt, agy) ~ NA,
+          height < 0.0725 & agy_n == 1 ~ NA,
+          .default = agy
+        )
       ),
-      filter = speckle,
-      colour = '#44446c',
-      proportion = 0.1
-    ) +
-    geom_treemap_text(
-      color = "gray10",
+      na.rm = TRUE,
       place = "bottomright",
-      start = "topleft",
-      # reflow = TRUE,
+      reflow = TRUE,
+      contrast = TRUE,
+      family = "Montserrat"
+    ) +
+    # subgroup border
+    geom_rect(data = tm_ce_subgroup,
+              fill = NA,
+              color = "white") +
+    # department label full or acronym
+    geom_fit_text(
+      data = tm_ce_subgroup,
+      mapping = aes(
+        label =
+          case_when(
+            width >= 0.25 ~ department,
+            width < 0.25 & width >= 0.050 ~ dpt,
+            .default = NA
+          )
+      ),
+      na.rm = TRUE,
+      place = "topleft",
+      reflow = TRUE,
+      hjust = 0,
+      formatter = toupper,
+      contrast = TRUE,
       family = "Montserrat",
-      layout = "srow"
+      fontface = "bold"
     ) +
-    geom_treemap_subgroup_border(
-      color = "white",
-      size = 1.75,
-      show.legend = FALSE,
-      start = "topleft",
-      layout = "srow"
-    ) +
-    geom_treemap_subgroup_text(
-      color = "white",
+    # department label, rotated
+    geom_fit_text(
+      data = tm_ce_subgroup,
+      mapping = aes(label = ifelse(height > width & width < 0.050, dpt, NA)),
+      na.rm = TRUE,
+      place = "center",
+      reflow = TRUE,
+      contrast = TRUE,
       family = "Montserrat",
       fontface = "bold",
-      place = "topleft",
-      start = "topleft",
-      layout = "srow"
-    )
+      angle = 90
+    ) +
+    # amount label, center
+    geom_fit_text(
+      data = tm_ce_subgroup,
+      mapping = aes(label = ifelse(height >= 0.0775, to_php(amt_dpt), NA)),
+      na.rm = TRUE,
+      place = "centre",
+      contrast = TRUE,
+      family = "Montserrat",
+      fontface = "bold"
+    ) +
+    # amount label, bottom
+    geom_fit_text(
+      data = tm_ce_subgroup,
+      mapping = aes(label = ifelse(height <= 0.075 & width >= 0.055,
+                                   to_php(amt_dpt), NA)),
+      na.rm = TRUE,
+      place = "bottom",
+      contrast = TRUE,
+      family = "Montserrat",
+      fontface = "bold"
+    ) +
+    coord_cartesian(clip = "off") +
+    # amount label, outside
+    geom_label_repel(
+      data = tm_ce_subgroup,
+      mapping = 
+        aes(x = x, y = y,
+            label = 
+              case_when(
+                between(height, 0.055, 0.08) & width <= 0.054 ~ to_php(amt_dpt),
+                height <= 0.055 & width <= 0.054 ~ glue("{dpt}: {to_php(amt_dpt)}"),
+                .default = NA
+                )
+      ), 
+      color = "white",
+      fill = tm_ce_subgroup$color,
+      size = 2.75,
+      family = "Montserrat",
+      segment.color = "white",
+      segment.linetype = "longdash",
+      segment.alpha = 0.80,
+      segment.size = 0.2,
+      # segment.curvature = -1e-20,
+      # segment.ncp = 3,
+      # segment.angle = 40,
+      segment.square = TRUE,
+      segment.inflect = TRUE,
+      force = 1,
+      force_pull = 0,
+      box.padding = 0.1,
+      label.padding = 0,
+      label.r = 0,
+      label.size = 0,
+      # xlim = c(0, NA),
+      ylim = c(-0.0275, -0.0325),
+      na.rm = TRUE,
+      show.legend = FALSE,
+      max.overlaps = Inf
+    ) +
+    # could also use coord_cartesian(expand = FALSE)
+    scale_y_continuous(expand = expansion(mult = c(0, 0))) +
+    scale_x_continuous(expand = expansion(mult = c(0, 0)))
   
   p + labs(
+    x = NULL,
+    y = NULL,
     title = glue(
       "**Philippine government's
       {.a Confidential Funds}, <fiscal_year>**",
@@ -157,36 +278,39 @@ gen_plots <- function(fiscal_year) {
       family = "Montserrat",
       color = "#9c9bdb",
       style = classic_style(),
-      width = 1
+      width = NA
     ),
-    aspect.ratio = 1,
+    # aspect.ratio = 1,
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    axis.line = element_blank(),
     panel.background = element_blank(),
+    panel.border = element_blank(),
+    panel.grid = element_blank(),
     plot.background = element_rect(fill = "#131343", colour = NA),
     plot.title = element_marquee(
-      size = 32,
+      size = 26,
       hjust = 0,
-      margin = margin(0, 0, 10, 0),
-      style = modify_style(
-        classic_style(),
-        "a",
-        weight = "bold",
-        color = "white",
-        background = "#fa7405",
-        padding = trbl(em(0.01))
-      )
+      margin = margin(0, 0, 0, 0),
+      style = cust_style,
+      width = 1
     ),
-    plot.title.position = "plot",
-    plot.subtitle = element_marquee(size = 12),
+    # plot.title.position = "plot",
+    plot.subtitle = element_marquee(
+      size = 12,
+      hjust = 0,
+      margin = margin(12, 0, 0, 0)
+    ),
     plot.caption = element_marquee(
       size = 10,
       hjust = 1,
-      margin = margin(t = 30, 0, 0, 0),
-      width = NA
+      margin = margin(20, 0, 0, 0)
     ),
-    plot.caption.position = "plot",
-    plot.tag = element_marquee(size = 10),
+    # plot.caption.position = "panel",
+    plot.tag = element_marquee(size = 10, margin = margin(20, 0, 0, 0)),
     plot.tag.position = "bottomleft",
-    plot.tag.location = "plot"
+    plot.tag.location = "plot",
+    plot.margin = margin(20, 20, 10, 20)
   )
   
   theme_set(theme_pv)
@@ -194,9 +318,9 @@ gen_plots <- function(fiscal_year) {
   p
   
   ggsave(
-    filename = glue("breakdown_{fiscal_year}.svg"),
-    device = svg,
-    path = "image/svg",
+    filename = glue("breakdown_{fiscal_year}.png"),
+    # device = svg,
+    path = "image/",
     scale = 2,
     width = 1080,
     height = 1350,
